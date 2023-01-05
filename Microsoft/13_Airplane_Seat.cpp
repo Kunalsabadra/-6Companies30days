15e283b4654118f73d69f101c3074769b20d317c
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-01-05 16:20:47 +0530

    Update 13_Airplane_Seat.cpp

--- Microsoft/13_Airplane_Seat.cpp
+++ Microsoft/13_Airplane_Seat.cpp
@@ -1,1 +1,12 @@
-
+//Explanation
+/* the nth person will have only two option at the end either to seat on his own seat or 
+on the seat one which are likely to happen equally
+*/
+//code
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n == 1) return 1.00000;
+        else return 0.50000;
+    }
+};
