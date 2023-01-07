cb35eb27aa8b6c04cd8a5b816992c95e73cbb483
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2023-01-07 15:41:08 +0530

    Update 05_Split_array.cpp

--- Goldman_Sachs/05_Split_array.cpp
+++ Goldman_Sachs/05_Split_array.cpp
@@ -1,3 +1,13 @@
+/*
+Using two map
+One for how many sequences have that ending word
+Another for how many numbers are unchecked
+Loop through every number
+If there is sequence before the number, we add the number to the seq
+If not we create a new seq using the number
+If there aren't two numbers behind to let us create new seq, return False
+
+*/
 class Solution {
 public:
     bool isPossible(vector<int>& nums) {
